dc49948bb8b49610fd74034b3c8f1dfe011e49e1
eddieciou <a589658gur@gmail.com>
2021-07-20 15:03:13 +0800

    Initial commit

---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("Hello Golang")
+}
+// 註解
+// 撰寫程式 > 建置(build) > 執行程式
+// 建置： go build 程式檔案的名稱 ex. go build hello.go
+// 執行程式： 輸入執行檔案的名稱
